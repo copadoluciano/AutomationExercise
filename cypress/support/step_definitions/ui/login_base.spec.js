60f0c157fcfadb2a43bc6781bb3b5d0f826d867a
Luciano Copado <copadoluciano@gmail.com>
2022-11-25 13:28:40 -0300

    refactor trades feature 2

--- cypress/support/step_definitions/ui/login_base.spec.js
+++ cypress/support/step_definitions/ui/login_base.spec.js
@@ -1,6 +1,11 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 require('cypress-xpath')
 
+Given('I go to sign-in and type {word}, {word} and {word}', function (email, password, secret) {
+    cy.login(Cypress.env(email), Cypress.env(password), Cypress.env(secret))
+})
+
+
 Given('the user is in sign-in and type {word}, {word} and {word}', function (email, password, secret) {
     cy.login(Cypress.env(email), Cypress.env(password), Cypress.env(secret))
 })
