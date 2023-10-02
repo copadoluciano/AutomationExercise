542e631e1fe4ceee7005e70f5a4c1b719369035f
Luciano Copado <copadoluciano@gmail.com>
2023-10-02 14:28:05 -0300

    New escenarios and improvements

--- cypress/e2e/ui/addProducts.feature
+++ cypress/e2e/ui/addProducts.feature
@@ -13,9 +13,11 @@ Feature: Products
         Scenario Outline: Add Products in Cart
             When the user add products on position <position1> and <position2> to the cart
             Then the products <position1> and <position2> are seen inside the cart
-            And the prices, quantities and totals are correct
+            And the prices, quantities and totals are correct in the <position1> and <position2>
 
             Examples:
                 | position1 | position2 |
                 |  1         | 2         |
+                |  3         | 4        |
+                |  5         | 6         |
 
--- cypress/support/step_definitions/ui/products.spec.js
+++ cypress/support/step_definitions/ui/products.spec.js
@@ -5,6 +5,7 @@ let productNames = [];
 let productPrices = [];
 
 
+
 Given('the user is at Home Page', function () {
     cy.visit("https://automationexercise.com/")
     cy.get('body').should('be.visible')
@@ -23,7 +24,7 @@ When('the user add products on position {word} and {word} to the cart', function
 
     // Guardar precios
     getValue("//IMG[@src='/get_product_picture/"+positions[i]+"']/following-sibling::h2").then((value) => {
-        productPrices[i] = value
+        productPrices[i] = value.replace(/[^0-9]/g, '');
         cy.log(productPrices[i])
     })
 }
@@ -32,45 +33,43 @@ When('the user add products on position {word} and {word} to the cart', function
 })
 
 Then('the products {word} and {word} are seen inside the cart', function (position1, position2) {
-    cy.xpath("//A[@href='/product_details/"+position1+"']").invoke('text').then(function (text) {
+    let positions = [position1, position2];
+    const cantPositions = positions.length
+
+    for (let i= 0; i<=cantPositions-1; i++){
+    cy.xpath("//A[@href='/product_details/"+positions[i]+"']").invoke('text').then(function (text) {
         let productNameCart1 = text;
-        cy.wrap(productNameCart1).should("eq", productNames[0]);
+        cy.wrap(productNameCart1).should("eq", productNames[i]);
 
     });
-    cy.xpath("//A[@href='/product_details/"+position2+"']").invoke('text').then(function (text) {
+    cy.xpath("//A[@href='/product_details/"+positions[i]+"']").invoke('text').then(function (text) {
         let productNameCart2 = text;
-        cy.wrap(productNameCart2).should("eq", productNames[1]);
+        cy.wrap(productNameCart2).should("eq", productNames[i]);
     });
+}
   
 })
 
-Then('the prices, quantities and totals are correct', function () {
-    cy.xpath("(//A[@href='/product_details/1'][text()='"+productNames[0]+"']/../../..//P)[2]").invoke('text').then(function (text) {
-        let productPriceCart1 = text;
-        cy.wrap(productPriceCart1).should("eq", productPrices[0]);
+Then('the prices, quantities and totals are correct in the {word} and {word}', function (position1, position2) {
+    let positions = [position1, position2];
+    const cantPositions = positions.length
+
+    for (let i= 0; i<=cantPositions-1; i++){
+    cy.xpath("(//A[@href='/product_details/"+positions[i]+"'][text()='"+productNames[i]+"']/../../..//P)[2]").invoke('text').then(function (text) {
+        let productPriceCart1 = text.replace(/[^0-9]/g, '');;
+        cy.wrap(productPriceCart1).should("eq", productPrices[i]);
 
     });
 
-    cy.xpath("(//A[@href='/product_details/2'][text()='"+productNames[1]+"']/../../..//P)[2]").invoke('text').then(function (text) {
-        let productPriceCart2 = text;
-        cy.wrap(productPriceCart2).should("eq", productPrices[1]);
+    cy.xpath("//A[@href='/product_details/"+positions[i]+"'][text()='"+productNames[i]+"']/../../..//BUTTON[@class='disabled'][text()='1']").contains('1').invoke('text').then(function (value) {
+    let quantity = value
+    cy.xpath("//A[@href='/product_details/"+positions[i]+"'][text()='"+productNames[i]+"']/../../..//P[@class='cart_total_price']").invoke('text').then(function (value) {
+        let price = parseInt(value.replace(/[^0-9]/g, ''));
+        cy.wrap(price).should("eq", productPrices[i]*quantity);
 
     });
-
-    cy.xpath("//A[@href='/product_details/1'][text()='"+productNames[0]+"']/../../..//BUTTON[@class='disabled'][text()='1']").contains('1')
-    cy.xpath("//A[@href='/product_details/2'][text()='"+productNames[1]+"']/../../..//BUTTON[@class='disabled'][text()='1']").contains('1')
-
-    cy.xpath("//A[@href='/product_details/1'][text()='"+productNames[0]+"']/../../..//P[@class='cart_total_price']").invoke('text').then(function (total) {
-        let total1 = total.replace(/[^0-9]/g, '');
-        cy.wrap(total1).should("eq", '500');
-
-    });
-
-    cy.xpath("//A[@href='/product_details/2'][text()='"+productNames[1]+"']/../../..//P[@class='cart_total_price']").invoke('text').then(function (total) {
-        let total1 = total.replace(/[^0-9]/g, '');
-        cy.wrap(total1).should("eq", '400');
-
-    });
-
+});
+    
+    }
     
 })
